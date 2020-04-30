ddc61e1a65cd5c8add4f59ef359e3f1358296169
SamualBanfield <samualbanfield@gmail.com>
2020-04-30 22:36:06 +1000

    Finished Methods & Functions

--- C#/Functions and Methods/Program.cs
+++ C#/Functions and Methods/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Functions_and_Methods
 {
@@ -40,22 +41,70 @@ namespace Functions_and_Methods
             int[] array = new int[5];
             for (int i = 0; i < 5; i++)
             {
-
+              Console.WriteLine();
+              Console.Write("Enter in a number: ");
+              array[i] = Convert.ToInt32(Console.ReadLine());
             }
+            Console.WriteLine("The sum of the array is {0}", arrayAdd(array));
             break;
           case 6:
+            Console.WriteLine();
+            Console.Write("Enter in a number: ");
+            int int1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Enter in another number: ");
+            int int2 = Convert.ToInt32(Console.ReadLine());
+            (int1, int2) = swapInts(int1, int2);
+            Console.WriteLine($"Now the 1st number is {int1} and the second number is {int2}");
             break;
           case 7:
+            Console.WriteLine();
+            Console.Write("Enter in a base number: ");
+            int baseNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Enter in exponent: ");
+            int exponent = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The number {0} ^ {1} = {2}", baseNum, exponent, powerOf(baseNum, exponent));
             break;
           case 8:
+            Console.WriteLine();
+            Console.Write("Enter in the length of fibonacci you wish to display: ");
+            int sequenceLen = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The fibonacci sequence for {0} numbers is:", sequenceLen);
+            Console.WriteLine(String.Join(", ", fibonacci(sequenceLen)));
+            Console.WriteLine();
             break;
           case 9:
+            Console.WriteLine();
+            Console.Write("Enter in a number: ");
+            int primeNum = Convert.ToInt32(Console.ReadLine());
+            bool primeBool = primeCheck(primeNum);
+            if (primeBool == true)
+            {
+              Console.WriteLine("{0} is a prime number!", primeNum);
+            }
+            if (primeBool == false)
+            {
+              Console.WriteLine("{0} is not a prime number!", primeNum);
+            }
             break;
           case 10:
+            Console.WriteLine();
+            Console.Write("Enter in a number: ");
+            int sumNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("'{0}' sum = {1}", sumNumber, sumOfNum(sumNumber));
             break;
           case 11:
+            Console.WriteLine();
+            Console.Write("Enter in a number: ");
+            int factorialNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} factorial = {1}", factorialNum, factorial(factorialNum));
             break;
           case 12:
+            Console.WriteLine();
+            Console.Write("Enter in a number: ");
+            int fibonacciPosition = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The fibonacci of the term {0} is: {1}", fibonacciPosition, fibonacciP(fibonacciPosition));
             break;
           default:
             System.Environment.Exit(0);
@@ -96,6 +145,96 @@ Have a nice day!
         }
       }
       return count;
+    }
+    static int arrayAdd(int[] array)
+    {
+      int counter = 0;
+      foreach (int num in array)
+      {
+        counter += num;
+      }
+      return counter;
+    }
+    static Tuple<int, int> swapInts(int int1, int int2)
+    {
+      (int1, int2) = (int2, int1);
+      return Tuple.Create(int1, int2);
+    }
+    static double powerOf(int baseNum, int exponent)
+    {
+      return Math.Pow(baseNum, exponent);
+    }
+    static List<int> fibonacci(int sequenceLen)
+    {
+      List<int> sequence = new List<int>();
+      for (int i = 0; i < sequenceLen; i++)
+      {
+        try
+        {
+          sequence.Add(sequence[i - 1] + sequence[i - 2]);
+        }
+        catch
+        {
+          sequence.Add(i);
+        }
+      }
+      return sequence;
+    }
+    static bool primeCheck(int n)
+    {
+      if (n <= 3)
+      {
+        return n > 1;
+      }
+      else if ((n % 2 == 0) || (n % 3 == 0))
+      {
+        return false;
+      }
+      for (int i = 5; i * i <= n;)
+      {
+        if ((n % i == 0) || (n % (i + 2) == 0))
+        {
+          return false;
+        }
+        i += 6;
+      }
+      return true;
+    }
+    static double sumOfNum(int n)
+    {
+      string stringN = n.ToString();
+      Console.WriteLine(stringN);
+      double total = 0;
+      for (int i = 0; i < stringN.Length; i++)
+      {
+        total += Char.GetNumericValue(stringN[i]);
+      }
+      return total;
+    }
+    static int factorial(int n)
+    {
+      int total = n;
+      for (int i = n - 1; i > 0; i--)
+      {
+        total *= i;
+      }
+      return total;
+    }
+    static int fibonacciP(int position)
+    {
+      List<int> sequence = new List<int>();
+      for (int i = 0; i < position; i++)
+      {
+        try
+        {
+          sequence.Add(sequence[i - 1] + sequence[i - 2]);
+        }
+        catch
+        {
+          sequence.Add(i);
+        }
+      }
+      return sequence[sequence.Count - 1];
     }
   }
 }
